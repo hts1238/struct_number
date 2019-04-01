e323ebf45e4d1151f9c9b7b7dd1ec4af019b25ef
Timur <hts1238@mail.ru>
2019-04-01 14:46:14 +0500

    refactoring

--- number.h
+++ number.h
@@ -7,53 +7,83 @@ using namespace std;
 
 struct Number {
 public:
-	int num;
-	int den;
+    int num;
+    int den;
 
-	Number() {
-		num = 0;
-		den = 1;
-	}
+    Number() {
+        num = 0;
+        den = 1;
+    }
 
-	Number(const int& n) {
-		num = n;
-		den = 1;
-	}
+    Number(const int& n) {
+        num = n;
+        den = 1;
+    }
 
-	Number(const int& n, const int& d) {
-		if (d == 0) {
-			cout << "Error: Division by zero at initialization. Denominator can not be zero\n";
-			exit(1);
-		}
-		num = n;
-		den = d;
-	}
+    Number(const int& n, const int& d) {
+        if (d == 0) {
+            cout << "Error: Division by zero at initialization. Denominator can not be zero\n";
+            exit(1);
+        }
+        num = n;
+        den = d;
 
-	double normal() {
-		return (double) num / den;
-	}
+        reduce();
+    }
+
+    double norm() {
+        return (double) num / den;
+    }
+
+    Number operator + (Number& o) {
+        return Number(num*o.den + den*o.num, den*o.den);
+    }
+
+    Number operator - (Number& o) {
+        Number n(-o.num, o.den);
+        return *this + n;
+    }
+
+    Number operator * (Number& o) {
+        
+    }
 
 private:
-	int gcd(int a, int b) {
-		while (b) {
-			a %= b;
-			swap(a, b);
-		}
-		return a;
-	}
+    void reduce() {
+        int _gcd = gcd(num, den);
+        num /= _gcd;
+        den /= _gcd;
+    }
 
-	int lcm(int& a, int& b) {
-		return a * b / gcd(a, b);
-	}
+    int gcd(int a, int b) {
+        a = abs(a);
+        b = abs(b);
+        
+        while (b) {
+            a %= b;
+            swap(a, b);
+        }
+        return a;
+    }
+
+    int lcm(int& a, int& b) {
+        return a * b / gcd(a, b);
+    }
 };
 
-ostream& operator << (ostream& cout, Number& n) {
-	if (n.den == 1) {
-		cout << n.num;
-	} else {
-		cout << n.num << "/" << n.den;
-	}
-	return cout;
+ostream& operator << (ostream& cout, const Number& n) {
+    if (n.den == 1) {
+        cout << n.num;
+    } else {
+        cout << n.num << "/" << n.den;
+    }
+    return cout;
+}
+
+istream& operator >> (istream& cin, Number& n) {
+    int val;
+    cin >> val;
+    n = Number(val);
 }
 
 #endif
